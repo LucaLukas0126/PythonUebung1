ceff8d3efb320f94c5996e7d330c72e64e906b01
lucalukas0126 <171wita17@o365.htl-leoben.at>
2018-06-04 16:40:20 +0200

    python datei3

--- Beispiel1.py
+++ Beispiel1.py
@@ -1,19 +1,25 @@
+def pruefziffer(code):
+    return 98 - (code&97)
 
-land="AT"
-blz="38282"
-konto= "3026713"
-check = "96"
-
-for i in range(len(konto), 11, 1):
-    konto = "0" + konto
-
-p1 = land + check
-p2 = blz[0:4]
-print(p1,p2)
+    land="AT"
+    blz="38282"
+    konto= "3026713"
 
 
+    for i in range(len(konto), 11):
+        konto = "0" + konto
 
-print("Kontonummer:",konto)
-print()
+    iban = blz+konto+"1029"+"00"
+    check = pruefziffer(iban)
 
 
+    p1 = land + check
+    p2 = blz[0:4]
+    p3 = blz[4:5]+konto[0:3]
+    p4 = konto[3:7]
+    p5 =konto[7:11]
+    print(p1,p2,p3,p4,p5)
+ 
+    #pruefziffer = blz + konto + "1029"+"00"
+    ##pruefziffer%97
+ 
