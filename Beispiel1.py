2eb75d6d911a78ffbe79de46fadfd554ce5bffd6
lucalukas0126 <171wita17@o365.htl-leoben.at>
2018-06-04 16:04:26 +0200

    python datei

--- Beispiel1.py
+++ Beispiel1.py
@@ -1,8 +1,19 @@
 
-print("Hallo Welt")
-print("local version")
-print("MARCO")
+land="AT"
+blz="38282"
+konto= "3026713"
+check = "96"
 
-print("Hello universe!")
-print("github Version")
+for i in range(len(konto), 11, 1):
+    konto = "0" + konto
+
+p1 = land + check
+p2 = blz[0:4]
+print(p1,p2)
+
+
+
+print("Kontonummer:",konto)
+print()
+
 
